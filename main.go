5f5d8ad0d580c8aeab9201593d0e8b8ecb23e7b0
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2022-01-15 22:19:04 +0100

    Add running ability

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"image/color"
 	_ "image/png"
 	"log"
 )
 
 const (
-	screenWidth  = 856
+	screenWidth  = 854
 	screenHeight = 480
 )
 
@@ -22,35 +23,52 @@ func init() {
 	}
 }
 
+type Player struct {
+	heroX float64
+	heroY float64
+	speed float64
+}
+
 type Game struct {
-	player struct {
-		heroX int
-		heroY int
-	}
+	player Player
 }
 
 func (g *Game) Update() error {
 
 	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
-		g.player.heroX = g.player.heroX - 1
+		g.player.heroX = g.player.heroX - g.player.speed
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
-		g.player.heroX = g.player.heroX + 1
+		g.player.heroX = g.player.heroX + g.player.speed
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
-		g.player.heroY = g.player.heroY - 1
+		g.player.heroY = g.player.heroY - g.player.speed
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
-		g.player.heroY = g.player.heroY + 1
+		g.player.heroY = g.player.heroY + g.player.speed
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyShiftLeft) {
+		g.player.speed = 2
+	} else {
+		g.player.speed = 1
 	}
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+
+	screen.Fill(color.NRGBA{
+		R: 25,
+		G: 25,
+		B: 25,
+		A: 255,
+	})
+
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(float64(g.player.heroX), float64(g.player.heroY))
 	screen.DrawImage(img, op)
@@ -61,13 +79,12 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func main() {
-	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Ebiten Demo")
+	ebiten.SetWindowResizable(true)
+
 	if err := ebiten.RunGame(&Game{
-		player: struct {
-			heroX int
-			heroY int
-		}{heroX: 0, heroY: 0},
+		player: Player{heroX: 0, heroY: 0, speed: 1},
 	}); err != nil {
 		log.Fatal(err)
 	}
